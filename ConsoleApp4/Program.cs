24146ad54d11cda1d7225e3893632f197accddf7
HP <HP@DESKTOP-ECOEFKE>
2022-08-31 08:09:35 -0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] apcfans = { 23, 17, 21, 31, 27, 18 };
+            int total = 0;
+            foreach (var youthAge in apcfans)
+            {
+                total = total + youthAge;
+             
+            }
+
+            int sum = total;
+            Console.WriteLine ($"The sum of apcfans is {total}");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
